5b9ab5c62933f6d4669792065d9a0b7cf2591339
GarethWoodman <gareth.woodman92@gmail.com>
2020-10-14 15:23:07 +0100

    Completed Question Model

---
+++ models/question.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var QuestionSchema = new Schema(
+  {
+    category: {type: Schema.Types.ObjectId, ref: 'Category', required: true},
+    content: {type: String, required: true},
+    incorrect_answers: [{type: String, required: true}],
+    correct_answer: {type: String, required: true},
+  }
+);
+
+module.exports = mongoose.model('Question', QuestionSchema);
