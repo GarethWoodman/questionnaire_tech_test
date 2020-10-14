12c71914ba379fd946a526799f0d52f204659258
GarethWoodman <gareth.woodman92@gmail.com>
2020-10-14 15:13:22 +0100

    Completed Student model

---
+++ models/student.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var StudentSchema = new Schema(
+  {
+    pin: {type: Number, required: true, maxlength: 6},
+    first_name: {type: String, required: true, maxlength: 100},
+    family_name: {type: String, required: true, maxlength: 100},
+  }
+);
+
+// Virtual for student's full name
+StudentSchema
+.virtual('name')
+.get(function () {
+  return this.first_name + ' ' + this.family_name;
+})
+
+module.exports = mongoose.model('Student', StudentSchema);
