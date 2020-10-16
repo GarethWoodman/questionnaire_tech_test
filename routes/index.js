85aa55064b058f453bd3deae15c4dd85d40a46c7
GarethWoodman <gareth.woodman92@gmail.com>
2020-10-16 10:04:34 +0100

    Refactored Index

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var category_controller = require('../controllers/categoryController')
 var question_controller = require('../controllers/questionController')
 var result_controller = require('../controllers/resultController')
 
-/* GET home page. */
+// Home Page
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
@@ -15,35 +15,35 @@ router.get('/', function(req, res, next) {
 router.get('/login', function(req, res, next) {
   res.render('login');
 })
-
 router.post('/login', student_controller.student_login)
 
-// router.get('/questions', function(req, res, next) {
-//   res.render('questions', { student: req.session.currentStudent, cateogry: req.session.currentCategory });
-// })
-
-router.get('/questions', question_controller.current_question);
-
+// Load Questions from category
 router.get('/category_questions', question_controller.category);
 
-router.get('/categories', function(req, res, next) {
-  res.render('categories')
-})
-
-router.post('/categories', category_controller.category_selection);
-
-router.get('/control_panel', function(req, res, next) {
-  res.render('control_panel')
-})
-
+// Question Form
+router.get('/questions', question_controller.current_question);
 router.post('/submit', question_controller.submit)
 
+// Load submission page after all answers submitted
 router.get('/submitted', function(req, res, next) {
   res.render('answers_submitted')
 })
 
+// Category Selection Form
+router.get('/categories', function(req, res, next) {
+  res.render('categories')
+})
+router.post('/categories', category_controller.category_selection);
+
+// Load Category Selection for student
 router.get('/student_categories', category_controller.student_categories)
 
+// Load Control Panel for teacher
+router.get('/control_panel', function(req, res, next) {
+  res.render('control_panel')
+})
+
+// Load all student results along with their categories
 router.get('/results', result_controller.list)
 
 module.exports = router;
