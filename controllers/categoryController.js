cbd05913f40154fc09e929b0e19608b93d85599a
GarethWoodman <gareth.woodman92@gmail.com>
2020-10-18 18:07:35 +0100

    Added feature to prevent student from re-doing a completed set/category of questions

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,5 +1,6 @@
 var Category = require('../models/category');
-var api = require('../api')
+var Result = require('../models/result');
+var api = require('../api');
 
 exports.category_selection = function(req, res) {
   console.log(req.body.category)
@@ -15,6 +16,16 @@ exports.student_categories = function(req, res) {
   Category.find({})
     .exec(function (err, categoryList){
       if(err || categoryList.length === 0) { return res.redirect('/categories') }
-      res.render('student_categories', {categories: categoryList, liveCategory: api.currentCategory})
+      hasCompleted(req, res, categoryList);
     });
+}
+
+function hasCompleted(req, res, categoryList) {
+  Result.find({category_id: api.currentCategory})
+    .exec(function (err, results){
+      let completed = false
+      if(err || categoryList.length === 0) { return res.redirect('/categories') }
+      results.forEach(function (result){ completed = (result.student_id == req.session.currentStudent._id) })
+      res.render('student_categories', {categories: categoryList, liveCategory: api.currentCategory, completed: completed})
+  });
 }
--- controllers/studentController.js
+++ controllers/studentController.js
@@ -2,11 +2,9 @@
 var Student = require('../models/student');
 
 exports.student_login = function(req, res) {
-  console.log(req.body.pin)
   Student.find({pin: req.body.pin})
     .exec(function (err, student) {
       if(err || student.length === 0) { return res.redirect('/login') }
-      console.log(student)
       req.session.currentStudent = student[0];
       res.redirect('/student_categories')
     })
