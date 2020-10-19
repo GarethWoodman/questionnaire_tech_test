7d546eb0ff6ddd7ceb7e2d4c0a284a4a75611b49
Gareth Woodman <47083071+GarethWoodman@users.noreply.github.com>
2020-10-19 16:02:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 mongoose.connect(
-  process.env.MONGODB_URI,
+  process.env.MONGODB_URI || "mongodb://localhost/questionnaire",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
